671aded83eb64425e982f8f887b94eb4821edd15
Daniel Salcedo <johandanielgs@ufps.edu.co>
2025-05-26 11:12:23 -0500

    making changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Blog from './components/home-page/Blog.jsx';
 import SingUp from './components/sign-up/SignUp.jsx';
 import SingIn from './components/login/SignIn.jsx';
-import './App.css'
+import UserProfile from "./components/user-profile-page/UserProfile.jsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/*" element={<Blog />} />
-                <Route path="/SingUp" element={<SingUp/>} />
-                <Route path="/SingIn" element={<SingIn />} />
+                <Route path="/sing-up" element={<SingUp/>} />
+                <Route path="/sing-in" element={<SingIn />} />
+                <Route path="/user-profile" element={<UserProfile/>} />
             </Routes>
         </BrowserRouter>
     );
