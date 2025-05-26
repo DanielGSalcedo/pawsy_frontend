42f0c2e839fdcca9e975dfacf757d8488824ed63
cesarcamiloig <>
2025-05-26 11:23:42 -0500

    Apps.jsx editado para ejecutar vista de PetEdit.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Blog from './components/home-page/Blog.jsx';
-import SingUp from './components/sign-up/SignUp.jsx';
-import SingIn from './components/login/SignIn.jsx';
-import './App.css'
+import PetEdit from './components/pet-edit/PetEdit'; // Asegúrate que el path sea correcto
+import './App.css';
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/*" element={<Blog />} />
-                <Route path="/SingUp" element={<SingUp/>} />
-                <Route path="/SingIn" element={<SingIn />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<PetEdit />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
