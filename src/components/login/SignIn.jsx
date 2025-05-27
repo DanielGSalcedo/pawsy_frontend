9bab5020cf13e19f68067c3773e9c646e8369679
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-05-27 15:29:42 -0500

    Added sign in validation function

--- src/components/login/SignIn.jsx
+++ src/components/login/SignIn.jsx
@@ -17,6 +17,7 @@ import AppTheme from '../shared-theme/AppTheme';
 import ColorModeSelect from '../shared-theme/ColorModeSelect';
 import { GoogleIcon, FacebookIcon, PawsyIcon } from '../sign-up/CustomIcons';
 import ForgotPassword from './ForgotPassword';
+import { userApi } from '../../scripts/userApi';
 
 const Card = styled(MuiCard)(({ theme }) => ({
   display: 'flex',
@@ -82,10 +83,13 @@ export default function SignIn(props) {
       return;
     }
     const data = new FormData(event.currentTarget);
-    console.log({
+    const user = {
       email: data.get('email'),
       password: data.get('password'),
-    });
+    };
+    console.log(user);
+    event.preventDefault();
+    handleSignIn(user);
   };
 
   const validateInputs = () => {
@@ -230,4 +234,16 @@ export default function SignIn(props) {
       </SignInContainer>
     </AppTheme>
   );
+}
+
+async function handleSignIn(user) {
+  try {
+      const response = await userApi.signIn(user);
+      console.log(response.token);
+      // window.location.href = '/';
+    } catch (error) {
+      console.error('Error signing in:', error);
+      alert('Error signing in. Please try again.');
+      return;
+    }
 }
