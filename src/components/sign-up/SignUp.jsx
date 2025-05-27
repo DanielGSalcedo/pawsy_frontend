1e12dde3933caf14e292f0a696fa07e35e326730
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-05-27 16:10:03 -0500

    Removed email updates button

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -215,10 +215,6 @@ export default function SignUp(props) {
                 color={passwordError ? 'error' : 'primary'}
               />
             </FormControl>
-            <FormControlLabel
-              control={<Checkbox value="allowExtraEmails" color="primary" />}
-              label="I want to receive updates via email."
-            />
             <Button
               type="submit"
               fullWidth
