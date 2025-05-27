8cbc237dbb7314baf20d032bcfa8f1e65c1fc187
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-05-27 16:09:17 -0500

    Removed third party signup options

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -228,37 +228,6 @@ export default function SignUp(props) {
               Sign up
             </Button>
           </Box>
-          <Divider>
-            <Typography sx={{ color: 'text.secondary' }}>or</Typography>
-          </Divider>
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-            <Button
-              fullWidth
-              variant="outlined"
-              onClick={() => alert('Sign up with Google')}
-              startIcon={<GoogleIcon />}
-            >
-              Sign up with Google
-            </Button>
-            <Button
-              fullWidth
-              variant="outlined"
-              onClick={() => alert('Sign up with Facebook')}
-              startIcon={<FacebookIcon />}
-            >
-              Sign up with Facebook
-            </Button>
-            <Typography sx={{ textAlign: 'center' }}>
-              Already have an account?{' '}
-              <Link
-                href="/material-ui/getting-started/templates/sign-in/"
-                variant="body2"
-                sx={{ alignSelf: 'center' }}
-              >
-                Sign in
-              </Link>
-            </Typography>
-          </Box>
         </Card>
       </SignUpContainer>
     </AppTheme>
