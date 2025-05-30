92b83c2723b23ee4bc89414346e19e0f23b9c55a
BryanVeraDev <bryanalejandrovo@ufps.edu.co>
2025-05-30 10:59:35 -0500

    Ruta agregada.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SingUp from "./components/sign-up/SignUp.jsx";
 import SingIn from "./components/login/SignIn.jsx";
 import PasswordReset from "./components/password-reset/PasswordReset.jsx";
 import UserProfile from "./components/user-profile-page/UserProfile.jsx";
+import PetProfile from "./components/pet-profile/PetProfile.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/sing-in" element={<SingIn />} />
         <Route path="/recover-password" element={<PasswordReset />} />
         <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/pet-profile" element={<PetProfile />} />
       </Routes>
     </BrowserRouter>
   );
