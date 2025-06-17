9006d5bb5b09edf3845f46e93232806681fb1b42
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2025-06-17 12:53:06 -0500

    editar perfil sirve

--- src/components/user-profile-page/EditProfile.jsx
+++ src/components/user-profile-page/EditProfile.jsx
@@ -131,7 +131,7 @@ export default function EditProfile() {
     //   navigate(`/perfil/${id}`);
     // }, 1000);
 
-    if (saveUserProfile(form)) {
+    if (saveUserProfile(form.nombre)) {
       alert("Cambios guardados correctamente");
       setSaving(false);
       navigate(`/perfil/${id}`);
